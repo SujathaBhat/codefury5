fdfef253d56f290911118bec9a45eaa3b8c49a1a
SujathaBhat <sujathabhat6@gmail.com>
2022-08-13 19:25:53 +0530

    added templates

--- startup_project/posts/urls.py
+++ startup_project/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='user-home'),
+    path('home/', views.home, name='posts-home'),
      path('about/', views.about, name='about'),
 ]
--- startup_project/posts/views.py
+++ startup_project/posts/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Siri',
+        'title': 'Post',
+        'content': 'First post content',
+        'date_posted': 'August 7, 2021'
+    },
+    {
+        'author': 'tom',
+        'title': 'jerry',
+        'content': 'Hi',
+        'date_posted': 'August 14, 2021'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>User Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'posts/about.html', {'title': 'About'})
